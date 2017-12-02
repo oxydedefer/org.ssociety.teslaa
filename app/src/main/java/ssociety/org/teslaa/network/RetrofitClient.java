99a0677781a12ea6b9aa421ee8520ef4101d5ee0
seb <sebastien@affinitic.be>
2017-12-02 16:46:52 +0100

    Add Retrofit client

---
+++ app/src/main/java/ssociety/org/teslaa/network/RetrofitClient.java
@@ -0,0 +1,22 @@
+package ssociety.org.teslaa.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by seb on 2/12/17.
+ */
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
