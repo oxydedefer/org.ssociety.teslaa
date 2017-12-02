08238172706f807bdace5ae82eb5dc0620b48cf7
seb <sebastien@affinitic.be>
2017-12-02 15:28:32 +0100

    Add robolectric and first test

---
+++ app/src/test/java/ssociety/org/teslaa/FirstSimpleTest.java
@@ -0,0 +1,21 @@
+package ssociety.org.teslaa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by seb on 2/12/17.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+public class FirstSimpleTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
